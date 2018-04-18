505d0e654200ad6d54f692136c17223a10bbe4ae
rybalkinsd <yan.brikl@gmail.com>
2018-04-18 15:30:07 +0300

    Settings fixed

--- src/main/webapp/js/Settings.js
+++ src/main/webapp/js/Settings.js
@@ -2,14 +2,14 @@ var ClusterSetting = function () {
     this.gameServer = {
         protocol: 'ws',
         host: 'localhost',
-        port: '8080',
+        port: '8090',
         path: '/events/connect'
     };
 
     this.matchMaker = {
         protocol: 'http',
         host: 'localhost',
-        port: '8090',
+        port: '8080',
         path: '/matchmaker/join'
     };
 };
